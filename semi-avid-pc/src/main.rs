0af7e68e85f0d511a686886bb345f42bfdc0ef65
a.stevan <antoine.stevan@isae-supaero.fr>
2023-11-28 12:46:15 +0100

    add logic to generate powers or use from file

--- semi-avid-pc/src/main.rs
+++ semi-avid-pc/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::prelude::*;
 use std::ops::Div;
 
 use ark_bls12_381::Bls12_381;
@@ -5,12 +7,13 @@ use ark_ec::pairing::Pairing;
 use ark_ff::PrimeField;
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
 use ark_poly_commit::kzg10::{Powers, KZG10};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
 use ark_std::test_rng;
 use rs_merkle::algorithms::Sha256;
 use rs_merkle::Hasher;
 
 use semi_avid_pc::{commit, field, prove, setup::trim, Block};
-use tracing::{debug, info};
+use tracing::{debug, info, warn};
 
 type UniPoly12_381 = DensePolynomial<<Bls12_381 as Pairing>::ScalarField>;
 
@@ -79,11 +82,51 @@ fn main() {
     let bytes = std::env::args().nth(1).unwrap().as_bytes().to_vec();
     let k: usize = std::env::args().nth(2).unwrap().parse().unwrap();
     let n: usize = std::env::args().nth(3).unwrap().parse().unwrap();
+    let generate_powers: bool = std::env::args().nth(4).unwrap().parse().unwrap();
+    let powers_file = std::env::args().nth(5).unwrap_or("powers.bin".to_string());
 
-    let powers = setup::<Bls12_381, UniPoly12_381>(bytes.len()).unwrap();
+    const COMPRESS: Compress = Compress::Yes;
+    const VALIDATE: Validate = Validate::Yes;
+
+    if generate_powers {
+        info!("generating new powers");
+        let powers = setup::<Bls12_381, UniPoly12_381>(bytes.len()).unwrap();
+
+        info!("serializing powers");
+        let mut serialized = vec![0; powers.serialized_size(COMPRESS)];
+        powers
+            .serialize_with_mode(&mut serialized[..], COMPRESS)
+            .unwrap();
+
+        info!("dumping powers into `{}`", powers_file);
+        let mut file = File::create(&powers_file).unwrap();
+        file.write_all(&serialized).unwrap();
+    }
+
+    info!("reading powers from file `{}`", powers_file);
+    let powers = if let Ok(serialized) = std::fs::read(&powers_file) {
+        info!("deserializing the powers from `{}`", powers_file);
+        Powers::<Bls12_381>::deserialize_with_mode(&serialized[..], COMPRESS, VALIDATE).unwrap()
+    } else {
+        warn!("could not read powers from `{}`", powers_file);
+        info!("regenerating temporary powers");
+        setup::<Bls12_381, UniPoly12_381>(bytes.len()).unwrap()
+    };
+
     let blocks = run::<Bls12_381, UniPoly12_381>(&bytes, k, n, powers).unwrap();
 
-    for block in blocks {
-        println!("{:?}", block);
+    for (i, block) in blocks.iter().enumerate() {
+        let filename = format!("blocks/block_{}.bin", i);
+        std::fs::create_dir_all("blocks/").unwrap();
+
+        info!("serializing block {}", i);
+        let mut serialized = vec![0; block.serialized_size(COMPRESS)];
+        block
+            .serialize_with_mode(&mut serialized[..], COMPRESS)
+            .unwrap();
+
+        info!("dumping serialized block to `{}`", filename);
+        let mut file = File::create(&filename).unwrap();
+        file.write_all(&serialized).unwrap();
     }
 }
