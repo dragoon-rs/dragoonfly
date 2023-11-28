8734bc60e2f7a15a49522be65bdf86ee0fc93042
a.stevan <antoine.stevan@isae-supaero.fr>
2023-11-28 15:55:54 +0100

    refactor `run` into `encode` in the lib

--- semi-avid-pc/src/lib.rs
+++ semi-avid-pc/src/lib.rs
@@ -4,8 +4,10 @@ use ark_ec::pairing::Pairing;
 use ark_ff::{BigInteger, Field, PrimeField};
 use ark_poly::DenseUVPolynomial;
 use ark_poly_commit::kzg10::{Commitment, Powers, Randomness, KZG10};
-
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use rs_merkle::algorithms::Sha256;
+use rs_merkle::Hasher;
+use tracing::{debug, info};
 
 pub mod fec;
 pub mod field;
@@ -93,6 +95,49 @@ where
     }
 
     Ok(proofs)
+}
+
+pub fn encode<E, P>(
+    bytes: &[u8],
+    k: usize,
+    n: usize,
+    powers: Powers<E>,
+) -> Result<Vec<Block<E>>, ark_poly_commit::Error>
+where
+    E: Pairing,
+    P: DenseUVPolynomial<E::ScalarField, Point = E::ScalarField>,
+    for<'a, 'b> &'a P: Div<&'b P, Output = P>,
+{
+    info!("encoding and proving {} bytes", bytes.len());
+
+    debug!("splitting bytes into polynomials");
+    let elements = field::split_data_into_field_elements::<E>(bytes, k);
+    let nb_polynomials = elements.len() / k;
+    let polynomials = field::build_interleaved_polynomials::<E, P>(&elements, nb_polynomials);
+    info!("data is composed of {} polynomials", polynomials.len());
+
+    debug!("transposing the polynomials to commit");
+    let polynomials_to_commit = (0..polynomials[0].coeffs().len())
+        .map(|i| P::from_coefficients_vec(polynomials.iter().map(|p| p.coeffs()[i]).collect()))
+        .collect::<Vec<P>>();
+
+    debug!("committing the polynomials");
+    let (commits, _) = commit(&powers, &polynomials_to_commit)?;
+
+    debug!("creating the {} evaluation points", n);
+    let points: Vec<E::ScalarField> = (0..n)
+        .map(|i| E::ScalarField::from_le_bytes_mod_order(&[i as u8]))
+        .collect();
+
+    debug!("hashing the {} bytes with SHA-256", bytes.len());
+    let hash = Sha256::hash(bytes);
+
+    debug!(
+        "proving the {} bytes and the {} polynomials",
+        bytes.len(),
+        polynomials.len()
+    );
+    prove::<E, P>(commits, hash, bytes.len(), polynomials, &points)
 }
 
 pub fn verify<E, P>(block: &Block<E>, verifier_key: &Powers<E>) -> bool
--- semi-avid-pc/src/main.rs
+++ semi-avid-pc/src/main.rs
@@ -1,65 +1,18 @@
 use std::io::prelude::*;
-use std::ops::Div;
 use std::process::exit;
 use std::{fs::File, path::PathBuf};
 
 use ark_bls12_381::Bls12_381;
 use ark_ec::pairing::Pairing;
-use ark_ff::PrimeField;
-use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
+use ark_poly::univariate::DensePolynomial;
 use ark_poly_commit::kzg10::Powers;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
-use rs_merkle::algorithms::Sha256;
-use rs_merkle::Hasher;
 use tracing::{debug, error, info, warn};
 
 use semi_avid_pc::verify;
-use semi_avid_pc::{commit, field, prove, setup, Block};
+use semi_avid_pc::{encode, setup, Block};
 
 type UniPoly12_381 = DensePolynomial<<Bls12_381 as Pairing>::ScalarField>;
-
-fn run<E, P>(
-    bytes: &[u8],
-    k: usize,
-    n: usize,
-    powers: Powers<E>,
-) -> Result<Vec<Block<E>>, ark_poly_commit::Error>
-where
-    E: Pairing,
-    P: DenseUVPolynomial<E::ScalarField, Point = E::ScalarField>,
-    for<'a, 'b> &'a P: Div<&'b P, Output = P>,
-{
-    info!("encoding and proving {} bytes", bytes.len());
-
-    debug!("splitting bytes into polynomials");
-    let elements = field::split_data_into_field_elements::<E>(bytes, k);
-    let nb_polynomials = elements.len() / k;
-    let polynomials = field::build_interleaved_polynomials::<E, P>(&elements, nb_polynomials);
-    info!("data is composed of {} polynomials", polynomials.len());
-
-    debug!("transposing the polynomials to commit");
-    let polynomials_to_commit = (0..polynomials[0].coeffs().len())
-        .map(|i| P::from_coefficients_vec(polynomials.iter().map(|p| p.coeffs()[i]).collect()))
-        .collect::<Vec<P>>();
-
-    debug!("committing the polynomials");
-    let (commits, _) = commit(&powers, &polynomials_to_commit)?;
-
-    debug!("creating the {} evaluation points", n);
-    let points: Vec<E::ScalarField> = (0..n)
-        .map(|i| E::ScalarField::from_le_bytes_mod_order(&[i as u8]))
-        .collect();
-
-    debug!("hashing the {} bytes with SHA-256", bytes.len());
-    let hash = Sha256::hash(bytes);
-
-    debug!(
-        "proving the {} bytes and the {} polynomials",
-        bytes.len(),
-        polynomials.len()
-    );
-    prove::<E, P>(commits, hash, bytes.len(), polynomials, &points)
-}
 
 fn main() {
     tracing_subscriber::fmt::try_init().expect("cannot init logger");
@@ -123,7 +76,7 @@ fn main() {
         exit(0);
     }
 
-    let blocks = run::<Bls12_381, UniPoly12_381>(&bytes, k, n, powers).unwrap();
+    let blocks = encode::<Bls12_381, UniPoly12_381>(&bytes, k, n, powers).unwrap();
 
     info!("dumping blocks to `{}`", BLOCK_DIR);
     for (i, block) in blocks.iter().enumerate() {
